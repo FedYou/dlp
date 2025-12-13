e6e1a54acfbb5add112dfd4ffbec65870b576538
FedYou <federicochurruarin0@gmail.com>
2025-12-12 22:10:49 -0300

    build: add build script

---
+++ scripts/build.ts
@@ -0,0 +1,34 @@
+import { build } from 'esbuild'
+import { execSync } from 'child_process'
+import fs from 'fs'
+
+import PACKAGE from '../package.json'
+
+const REQUIRE = 'import{createRequire}from"module";var require=createRequire(import.meta.url);'
+
+build({
+  entryPoints: ['src/main.ts'],
+  bundle: true,
+  minify: true,
+  outfile: 'dist/bin/dlp',
+  platform: 'node',
+  target: 'node20',
+  format: 'esm',
+  external: ['blessed', 'clipboardy'],
+  banner: {
+    js: `#!/usr/bin/env node\n;${REQUIRE}`
+  }
+}).catch(() => process.exit(1))
+
+execSync('chmod +x dist/bin/dlp')
+
+const _package = PACKAGE
+
+_package.workspaces = undefined as any
+_package.devDependencies = undefined as any
+_package.scripts = undefined as any
+_package.dependencies['dlp-core'] = undefined as any
+
+fs.writeFileSync('dist/package.json', JSON.stringify(_package, null, 2))
+fs.copyFileSync('README.md', 'dist/README.md')
+fs.copyFileSync('LICENSE', 'dist/LICENSE')
