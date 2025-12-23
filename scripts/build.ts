2b71287f7fcf1d24c0779a258baf180a92faa179
FedYou <federicochurruarin0@gmail.com>
2025-12-23 02:14:53 -0300

    build: update build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -6,6 +6,10 @@ import PACKAGE from '../package.json'
 
 const REQUIRE = 'import{createRequire}from"module";var require=createRequire(import.meta.url);'
 
+if (fs.existsSync('dist')) {
+  fs.rmSync('dist', { recursive: true })
+}
+
 buildSync({
   entryPoints: ['src/main.ts'],
   bundle: true,
@@ -30,6 +34,17 @@ _package.scripts = undefined as any
 _package.dependencies['dlp-core'] = undefined as any
 
 fs.writeFileSync('dist/package.json', JSON.stringify(_package, null, 2))
-fs.copyFileSync('README.md', 'dist/README.md')
-fs.cpSync('images', 'dist/images', { recursive: true })
 fs.copyFileSync('LICENSE', 'dist/LICENSE')
+
+const URL_IMAGES = {
+  screenshot1: 'https://raw.githubusercontent.com/FedYou/dlp/refs/heads/main/images/screenshot-1.png',
+  screenshot2: 'https://raw.githubusercontent.com/FedYou/dlp/refs/heads/main/images/screenshot-2.png'
+}
+
+let readmeContent = fs.readFileSync('README.md').toString()
+
+readmeContent = readmeContent
+  .replace('./images/screenshot-1.png', URL_IMAGES.screenshot1)
+  .replace('./images/screenshot-2.png', URL_IMAGES.screenshot2)
+
+fs.writeFileSync('dist/README.md', readmeContent)
